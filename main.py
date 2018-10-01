9a051e3a257f8361082ef38106c6795ca57abb37
Cupcakerainbow <Offendasmanyasucan@gmail.com>
2018-10-01 20:53:17 +0530

    Update main.py
    comment added

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ glutInitWindowSize(display_height, display_width)
 glutInitWindowPosition(100, 100)
 glutCreateWindow(title)
 init()
-#Display Fun
+#Display
 glutDisplayFunc(display)
 glutReshapeFunc(reshape)
 glutKeyboardFunc(keyboard)
