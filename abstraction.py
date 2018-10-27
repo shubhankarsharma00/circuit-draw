e2ed18a8e4ec3d98038a27afef3d8052bde7bab0
shubhankarsharma00 <shubhankarsharma78@gmail.com>
2018-10-27 19:13:42 +0530

    all work flow for and gate

--- abstraction.py
+++ abstraction.py
@@ -1,4 +1,6 @@
-flag_arr=[1,0,0]
+flag_bfs=[0,0]
+flag_arr=[1,0,0,0]
+on,off=[],[]
 import sys
 import math
 from OpenGL.GLUT import *
@@ -6,39 +8,131 @@ from OpenGL.GL import *
 from OpenGL.GLU import *
 from display_settings import *
 and_gates=[]
-v_wire=[]
+v_wire_down=[]
+v_wire_up=[]
 h_wire=[]
+edges = (
+    (0,1),
+    (1,2),
+    (2,3),
+    (0,3)
+    )
+g={}
+for x in xrange(display_height):
+    for y in xrange(display_width):
+        g[(x,y)]=[]
+
+visited={}
+for x in xrange(display_height):
+    for y in xrange(display_width):
+        visited[(x,y)]=False
+
+def swi_open((x,y)):
+    if(on.count((x,y))==0):
+        on.append((x,y))
+
+def swi_off((x,y)):
+    if(off.count((x,y))==0):
+        off.append((x,y))
+
+def clear():
+    for x in xrange(display_height):
+        for y in xrange(display_width):
+            visited[(x,y)]=False
 
 
-
+def bfs((x,y)):
+    global flag_bfs
+    if(flag_bfs[1]==1):
+        swi_off((x,y))
+        if(on.count((x,y))>0):
+            on.remove((x,y))
+        queue=[(x,y)]
+        while(len(queue)!=0):
+            p=queue[0]
+            visited[p]=True
+            queue.remove(p)
+            for i in g[p]:    
+                if(visited[i]==False):
+                    queue.append(i)
+                    visited[i]=True
+                    bfs(i)
+    else:
+        swi_open((x,y))
+        if(off.count((x,y))>0):
+            off.remove((x,y))
+        queue=[(x,y)]
+        while(len(queue)!=0):
+            p=queue[0]
+            visited[p]=True
+            queue.remove(p)
+            for i in g[p]:    
+                if(visited[i]==False):
+                    queue.append(i)
+                    visited[i]=True
+                    bfs(i)
+            
 
 def draw_Wire_vertical():
-    for t in v_wire:
+    for t in v_wire_up:
         # glClear(GL_COLOR_BUFFER_BIT)
         x=t[0]
-        y=t[1]
+        y=t[1]-40
         glBegin(GL_LINES)
-        glColor3fv((1,1,1))
-        glVertex2fv((x+40,display_width-(y-20)))
-        glVertex2fv((x+40,display_width-(y+20)))    
+        if(on.count(t)>0):
+            glColor3fv((0,1,0))
+        elif(off.count(t)>0):
+            glColor3fv((1,0,0))
+        else:    
+            glColor3fv((1,1,1))
+        glVertex2fv((x+20,display_width-(((y+40))+20)))
+        glVertex2fv((x+40,display_width-(((y+40))+20)))    
+        glVertex2fv((x,display_width-((y+80)+20)))
+        glVertex2fv((x+20,display_width-((y+80)+20)))            
+        glVertex2fv((x+20,display_width-((y+80)-20)))
+        glVertex2fv((x+20,display_width-((y+80)+20)))    
+        glEnd()
+    for t in v_wire_down:
+        # glClear(GL_COLOR_BUFFER_BIT)
+        x=t[0]
+        y=t[1]-40
+        glBegin(GL_LINES)
+        if(on.count(t)>0):
+            glColor3fv((0,1,0))
+        elif(off.count(t)>0):
+            glColor3fv((1,0,0))
+        else:    
+            glColor3fv((1,1,1))
+        glVertex2fv((x+20,display_width-((y+40)-20)))
+        glVertex2fv((x+20,display_width-((y+40)+20)))    
+        glVertex2fv((x,display_width-(y+20)))
+        glVertex2fv((x+20,display_width-(y+20)))    
+        glVertex2fv((x+20,display_width-((y+40)+20)))
+        glVertex2fv((x+40,display_width-((y+40)+20)))            
         glEnd()
     # glFlush()
-
 
 def draw_Wire_horizontal():
     for t in h_wire:
+        # print "hey"
         # glClear(GL_COLOR_BUFFER_BIT)
         x=t[0]
         y=t[1]
+        # print t,on.count(t),off.count(t)
+        if(on.count(t)>0 or on.count((x+40,y))>0 or on.count((x-40,y))>0):
+            glColor3fv((0,1,0))
+            swi_open((x,y))
+        elif(off.count(t)>0 or off.count((x+40,y))>0 or off.count((x-40,y))>0):
+            glColor3fv((1,0,0))
+            swi_off((x,y))
+        else:    
+            glColor3fv((1,1,1))
         glBegin(GL_LINES)
-        glColor3fv((1,1,1))
         glVertex2fv((x,display_width-(y+20)))
-        glVertex2fv((x+40,display_width-(y+20)))    
-        glEnd()
+        glVertex2fv((x+40,display_width-(y+20)))            
+        glEnd()     
+    
     # glFlush()
-
-
-
 
 def drawAnd():
     for t in and_gates:
@@ -52,9 +146,56 @@ def drawAnd():
             glVertex2d(x+t[0]+25,display_width-(y+t[1]+20))
         glEnd()
         # glFlush()
-        glBegin(GL_QUADS)
+        x=t[0]-40
+        y=t[1]-40
+        glBegin(GL_LINES)
+        if(on.count((x-40,y))>0):
+            glColor3fv((0,1,0))
+        elif(off.count((x-40,y))>0):
+            glColor3fv((1,0,0))
+        else:
+            glColor3fv((1,1,1))
+        glVertex2fv((x+20,display_width-((y+40)-20)))
+        glVertex2fv((x+20,display_width-((y+35)+20)))    
+        # glVertex2fv((x,display_width-(y+35)))
+        # glVertex2fv((x+40,display_width-(y+20)))    
+        glVertex2fv((x,display_width-(y+20)))
+        glVertex2fv((x+20,display_width-(y+20)))    
+        glVertex2fv((x+20,display_width-((y+40)+15)))
+        glVertex2fv((x+40,display_width-((y+40)+15)))            
+
+        if(on.count((x-40,y+80))>0):
+            glColor3fv((0,1,0))
+        elif(off.count((x-40,y+80))>0):
+            glColor3fv((1,0,0))
+        else:
+            glColor3fv((1,1,1))
+        glVertex2fv((x+20,display_width-((y+85)-20)))
+        glVertex2fv((x+20,display_width-((y+80)+20)))    
+        # glVertex2fv((x,display_width-(y+35)))
+        # glVertex2fv((x+40,display_width-(y+20)))    
+        glVertex2fv((x+20,display_width-(((y+40))+25)))
+        glVertex2fv((x+40,display_width-(((y+40))+25)))    
+        glVertex2fv((x,display_width-((y+80)+20)))
+        glVertex2fv((x+20,display_width-((y+80)+20)))            
+        if(on.count((x-40,y))>0 and on.count((x-40,y+80))>0):
+            glColor3fv((0,1,0))
+            swi_open((x+80,y+40))
+        elif(off.count((x-40,y))>0 or off.count((x-40,y+80))>0):
+            glColor3fv((1,0,0))
+            if on.count((x+80,y+40)):
+                on.remove((x+80,y+40))
+            swi_off((x+80,y+40))
+
+        else:                
+            glColor3fv((1,1,1))
+
+        glVertex2fv((x+80,display_width-(y+60)))
+        glVertex2fv((x+120,display_width-(y+60)))            
+        glEnd()
         x=t[0]
         y=t[1]
+        glBegin(GL_QUADS)
         for i in range(100):
             glColor3fv((1,1,1))
             glVertex2f(x,display_width-(y))
@@ -64,16 +205,9 @@ def drawAnd():
         glEnd()
         # glFlush()
 
-
-
-
-
 def init():
     glClearColor(0.0, 0.0, 0.0, 0.0)
     glShadeModel(GL_FLAT)
-
-
-
 
 
 def grid():
@@ -99,9 +233,6 @@ def grid():
             i+=40
         j+=40
 
-
-
-
 def backGround():
     glClear(GL_COLOR_BUFFER_BIT)
     glBegin(GL_QUADS)
@@ -112,12 +243,8 @@ def backGround():
     glVertex2f( 0.0, display_width)
     glEnd()
 
-
-
-
 def display():
-    global flag_arr
-    # print flag_arr[0]
+    # print on,off
     backGround()
     grid()
     # glClear(GL_COLOR_BUFFER_BIT)
@@ -127,10 +254,6 @@ def display():
         # glFlush()# print i,j
     glFlush()
 
-
-
-
-    
 def reshape(w, h):
     glViewport(0, 0, w, h)
     glMatrixMode(GL_PROJECTION)
@@ -139,62 +262,102 @@ def reshape(w, h):
 
 def keyboard(key, x, y):
     global flag_arr
+    global flag_bfs
     if key == chr(27):
         sys.exit(0)
-    if key == chr(48):
-        for i in range(3):
+    if key =='a':
+        for i in range(4):
             flag_arr[i]=0
         flag_arr[0]=1
         glutPostRedisplay()
-    if key == chr(49):
-        for i in range(3):
+    elif key =='V':
+        for i in range(4):
             flag_arr[i]=0
         flag_arr[1]=1
         glutPostRedisplay()
-    if key == chr(50):
-        for i in range(3):
+    elif key =='h':
+        for i in range(4):
             flag_arr[i]=0
         flag_arr[2]=1
         glutPostRedisplay()    
+    elif key == 'v':
+        for i in range(4):
+            flag_arr[i]=0
+        flag_arr[3]=1
+        glutPostRedisplay()    
+    elif key == 'g':
+        for i in range(4):
+            flag_arr[i]=0
+        flag_bfs[0]=1 
+        glutPostRedisplay()    
+    elif key == 'r':
+        for i in range(4):
+            flag_arr[i]=0
+        flag_bfs[1]=1 
+        glutPostRedisplay()    
 def none():
     pass
+    
 def spinDisplay():
     glutPostRedisplay()
 
-
 def mouse(button, state, x, y):
     global and_gates
-    global v_wire
+    global v_wire_down
+    global v_wire_up
     global h_wire
+    global flag_bfs
     x=float(int(x/40))*40
     y=float(int(y/40))*40
     if button == GLUT_LEFT_BUTTON:
-        print flag_arr
         if(state == GLUT_DOWN):
-            if flag_arr[0]==1:
+            # print flag_arr
+            if flag_bfs[0]==1:
+                clear()
+                bfs((x,y))
+                flag_bfs[0]=0
+                print sorted(on),sorted(off)
+            elif flag_bfs[1]==1:
+                clear()
+                bfs((x,y))
+                flag_bfs[1]=0
+                print sorted(on),sorted(off)
+            elif flag_arr[0]==1:
                 and_gates.append((x,y))
             elif flag_arr[1]==1:
-                v_wire.append((x,y))
+                v_wire_down.append((x,y))
+                g[(x,y)].append((x-40,y-40))
+                g[(x,y)].append((x+40,y))
+                g[(x-40,y-40)].append((x,y))
+                g[(x+40,y)].append((x,y))
+            elif flag_arr[3]==1:
+                v_wire_up.append((x,y))
+                g[(x,y)].append((x-40,y+40))
+                g[(x,y)].append((x+40,y))
+                g[(x-40,y+40)].append((x,y))
+                g[(x+40,y)].append((x,y))
             elif flag_arr[2]==1:
                 h_wire.append((x,y))
+                g[(x,y)].append((x+40,y))
+                g[(x,y)].append((x-40,y))
+                g[(x+40,y)].append((x,y))
+                g[(x+40,y)].append((x-40,y))
+                g[(x-40,y)].append((x,y))
+                g[(x-40,y)].append((x+40,y))
             glutIdleFunc(spinDisplay)
     elif button == GLUT_MIDDLE_BUTTON or button == GLUT_RIGHT_BUTTON:
         if(state == GLUT_DOWN):
             if and_gates.count((x,y))>0:
                 and_gates.remove((x,y))
-            if v_wire.count((x,y))>0:    
-                v_wire.remove((x,y))
             if h_wire.count((x,y))>0:    
                 h_wire.remove((x,y))
+            if on.count((x,y))>0:
+                on.remove((x,y))
+            if off.count((x,y))>0:
+                off.remove((x,y))
+            if v_wire_up.count((x,y))>0:    
+                v_wire_up.remove((x,y))
+            if v_wire_down.count((x,y))>0:    
+                v_wire_down.remove((x,y))
             # print "*"         
             glutIdleFunc(spinDisplay)
-
-
-
-
-edges = (
-    (0,1),
-    (1,2),
-    (2,3),
-    (0,3)
-    )
